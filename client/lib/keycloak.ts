14726908ba78ade6b6fbe2a84e718f51c974c6c6
Teja <tejasimma033@gmail.com>
2025-06-04 15:39:36 +0530

    update: server basic files

--- client/lib/keycloak.ts
+++
@@ -1,9 +0,0 @@
-export function getToken() {
-  const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-    const [key, val] = cookie.split('=');
-    acc[key] = decodeURIComponent(val);
-    return acc;
-  }, {} as Record<string, string>);
-  
-  return cookies['access_token'];
-}
